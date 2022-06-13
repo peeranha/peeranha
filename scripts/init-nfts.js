3c2e531b90f46a5fa98511acbf70e5c1b2038e3a
Yury Lukashevich <57390905+YuriyLu@users.noreply.github.com>
2022-06-13 04:09:42 +0300

    Fix images (#61)

--- scripts/init-nfts.js
+++ scripts/init-nfts.js
@@ -14,17 +14,21 @@ function getIpfsApiTheGraph() {
 }
 
 async function saveTextTheGraph(buf) {
-  await getIpfsApiTheGraph().add(buf);
+  const graphSaveResult = await getIpfsApiTheGraph().add(buf);
+  console.log(`Saved file to the graph IPFS - ${graphSaveResult.cid.toString()}`)
 }
 
-async function saveFileTheGraph(buf) {
-  await getIpfsApiTheGraph().add(buf);
+async function saveIpfsText(buf) {
+  const saveResult = await getIpfsApi().add(buf);
+  console.log(`Saved file to our IPFS - ${saveResult.cid.toString()}`)
+  return saveResult;
 }
 
 async function saveText(text) {
   const buf = Buffer.from(text, 'utf8');
   const saveResult = await getIpfsApi().add(buf);
   await saveTextTheGraph(buf);
+
   return saveResult.cid.toString();
 }
 
@@ -62,14 +66,16 @@ async function main() {
 async function initAchievement(peeranhaUser) {
   for (const { maxCount, lowerBound, type, path, name, description, attributes } of achievements("prod")) {
     const buffer = fs.readFileSync(path)
-    const imgHash = await saveFile(buffer)
+    const imgHash = "ipfs://" + await saveFile(buffer);
     let nft = {
       name: name,
       description: description,
       image: imgHash,
       attributes: attributes,
     };
-    const tx = await peeranhaUser.configureNewAchievement(maxCount, lowerBound, await getBytes32FromData(nft), type);
+    console.log(`init NFT ${name}`);
+    const nftIPFS = "ipfs://" + await saveText(JSON.stringify(nft));
+    const tx = await peeranhaUser.configureNewAchievement(maxCount, lowerBound, nftIPFS, type);
     console.log(`Sent transaction ${tx.hash} to init NFT ${name}`);
 
     console.log(`Waiting for confirmation`)
