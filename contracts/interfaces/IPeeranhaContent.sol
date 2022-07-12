fcdd02173104184e0800d312dbf8a5cadb1f7565
Yury Lukashevich <57390905+YuriyLu@users.noreply.github.com>
2022-07-12 04:08:09 +0300

    Hotfix/peer 99 (#67)

--- contracts/interfaces/IPeeranhaContent.sol
+++ contracts/interfaces/IPeeranhaContent.sol
@@ -9,12 +9,11 @@ interface IPeeranhaContent {
     function createReply(uint256 postId, uint16 parentReplyId, bytes32 ipfsHash, bool isOfficialReply) external;
     function createComment(uint256 postId, uint16 parentReplyId, bytes32 ipfsHash) external;
     function editPost(uint256 postId, bytes32 ipfsHash, uint8[] memory tags) external;
-    function editReply(uint256 postId, uint16 parentReplyId, bytes32 ipfsHash ) external;
+    function editReply(uint256 postId, uint16 parentReplyId, bytes32 ipfsHash, bool isOfficialReply) external;
     function editComment(uint256 postId, uint16 parentReplyId, uint8 commentId, bytes32 ipfsHash) external;
     function deletePost(uint256 postId) external;
     function deleteReply(uint256 postId, uint16 replyId) external;
     function deleteComment(uint256 postId, uint16 parentReplyId, uint8 commentId) external;
-    function changeStatusOfficialReply(uint256 postId, uint16 replyId) external;
     function changeStatusBestReply(uint256 postId, uint16 replyId) external;
     function voteItem(uint256 postId, uint16 replyId, uint8 commentId, bool isUpvote) external;
     function changePostType(uint256 postId, PostLib.PostType postType) external;
