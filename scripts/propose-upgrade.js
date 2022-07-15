2f32edd3b037d5a579cc4650671d7b3f539a7d77
Sergey Ilin <ilinserg@gmail.com>
2022-07-14 22:31:01 -0500

    Upgraded test env contracts

--- scripts/propose-upgrade.js
+++ scripts/propose-upgrade.js
@@ -9,12 +9,12 @@ async function main() {
 
   const PeeranhaUser = await ethers.getContractFactory("PeeranhaUser");
   console.log("Proposing upgrade for PeeranhaUser...");
-  const peeranhaUser = await defender.proposeUpgrade(USER_ADDRESS, PeeranhaUser);
+  const peeranhaUser = await defender.proposeUpgrade(USER_ADDRESS, PeeranhaUser, {timeout: 0});
   console.log("Peeranha User proposal at:", peeranhaUser.url);
 
   const PeeranhaCommunity = await ethers.getContractFactory("PeeranhaCommunity");
   console.log("Proposing upgrade for PeeranhaCommunity...");
-  const peeranhaCommunity = await defender.proposeUpgrade(COMMUNITY_ADDRESS, PeeranhaCommunity);
+  const peeranhaCommunity = await defender.proposeUpgrade(COMMUNITY_ADDRESS, PeeranhaCommunity, {timeout: 0});
   console.log("Peeranha Community proposal at:", peeranhaCommunity.url);
 
   const PeeranhaContent = await ethers.getContractFactory("PeeranhaContent", {
@@ -24,17 +24,17 @@ async function main() {
   });
 
   console.log("Proposing upgrade for PeeranhaContent...");
-  const peeranhaContent = await defender.proposeUpgrade(CONTENT_ADDRESS, PeeranhaContent, {unsafeAllow: ["external-library-linking"]});
+  const peeranhaContent = await defender.proposeUpgrade(CONTENT_ADDRESS, PeeranhaContent, {unsafeAllow: ["external-library-linking"], timeout: 0});
   console.log("PeeranhaContent proposal at:", peeranhaContent.url);
   
   const PeeranhaNFT = await ethers.getContractFactory("PeeranhaNFT");
   console.log("Proposing upgrade for PeeranhaNFT...");
-  const peeranhaNFT = await defender.proposeUpgrade(NFT_ADDRESS, PeeranhaNFT);
+  const peeranhaNFT = await defender.proposeUpgrade(NFT_ADDRESS, PeeranhaNFT, {timeout: 0});
   console.log("Peeranha NFT proposal at:", peeranhaNFT.url);
 
   const PeeranhaToken = await ethers.getContractFactory("PeeranhaToken");
   console.log("Proposing upgrade for PeeranhaToken...");
-  const peeranhaToken = await defender.proposeUpgrade(TOKEN_ADDRESS, PeeranhaToken);
+  const peeranhaToken = await defender.proposeUpgrade(TOKEN_ADDRESS, PeeranhaToken, {timeout: 0});
   console.log("Peeranha token proposal at:", peeranhaToken.url);
 }
 
