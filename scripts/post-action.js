585d21d7d7d109bc124ca55bb57fa19be94c7099
Freitag-fri <45832239+Freitag-fri@users.noreply.github.com>
2024-05-21 12:37:49 +0300

    update-post-action (#136)

--- scripts/post-action.js
+++ scripts/post-action.js
@@ -25,7 +25,7 @@ const SAVE_FILE_SERVICE = "save-file"
 async function getBytes32FromData(data) {
   const convertData = { content: Buffer.from(JSON.stringify(data), 'utf8'), encoding: 'Buffer' }
   const ipfsHash = await saveDataIpfsS3(convertData);
-  console.log("Uploaded file to IPFS - " + ipfsHash);
+  console.log("Uploaded file to IPFS - " + JSON.stringify(ipfsHash));
   return getBytes32FromIpfsHash(ipfsHash);
 }
 
@@ -110,12 +110,34 @@ const testDocumentating = {
 };
 
 async function getError() {
-  const response = `[\"0x02f8b4830138818263a78459c815c88459c815c883014c95949b53b80f03d4191be79085c07ffedda3671f6a4680b844522f4bf80000000000000000000000009fbe2c1d7b0ebeddb2faef30be00ed838f19e499000000000000000000000000000000000000000000000000000000000000000bc0809ff5d155cb2509e1f0519ed05dca538be51f3ddc2ff193bf952dd9bb726a5263a0060cc52eed84f4a4f2c7862e5d7710e40122b9b414e9446a0fcdca07682bc1fb\"]`
-  const parsed = ethers.utils.toUtf8String(Object.values(response))
-  console.log(parsed)
+  const tx = await ethers.provider.getTransaction("0x7517b92b7584fdbc4d36ff4a72f7412f4bd49839690eca42ae564be1522ba792");
+  // console.log(tx);
+
+  const unsignedTx = {
+      to: tx.to,
+      nonce: tx.nonce,
+      gasLimit: tx.gasLimit,
+      gasPrice: tx.gasPrice,
+      data: tx.data,
+      value: tx.value,
+      chainId: tx.chainId
+  };
+  const signature = {
+      v: tx.v,
+      r: tx.r,
+      s: tx.s
+  }
+
+  const raw = ethers.utils.serializeTransaction(unsignedTx, signature);
+  console.log(raw);
+
+  const txLog = ethers.utils.parseTransaction(raw)
+  console.log(txLog);
+
 }
 
 async function main() {
+  // await getError()
   await contentFunctions();
   // await userFunctions();
   // await communityFunctions();
@@ -131,23 +153,28 @@ async function userFunctions() {
   const peeranhaUser = await PeeranhaUser.attach(USER_ADDRESS);
 
   const txObj = await peeranhaUser.createUser(signers[0].address, await getBytes32FromData(testAccount));
+  // const txObj = await peeranhaUser.isUserExists(signers[0].address);
   // const txObj = await peeranhaUser.updateUser(signers[0].address, await getBytes32FromData(testAccount));
   // const txObj = await peeranhaUser.addUserRating("0x31339c62C0A44b875297945edb93D88092b5fa91", 100, 2);
-  // const txObj = await peeranhaUser.giveAdminPermission("0x570895Fd1f7d529606E495885f6EAF1924BAa08e")
   // const txObj = await peeranhaUser.giveCommunityModeratorPermission("0xE902761E0207A8470caA51FA11f397069FdADa2b", 2);
-  // const txObj = await peeranhaUser.grantRole(PROTOCOL_ADMIN_ROLE, "0xf5800B1a93C4b0A87a60E9751d1309Ce93CC0D3A")
-  // const txObj = await peeranhaUser.grantRole(DISPATCHER_ROLE, "0xF5DFa78C158697f39a3D425F7Ad4f9fE4154c0F9")
-  // const txObj = await peeranhaUser.grantRole(BOT_ROLE, "0xF5DFa78C158697f39a3D425F7Ad4f9fE4154c0F9")
-  // const txObj = await peeranhaUser.grantRole(PROTOCOL_ADMIN_ROLE, "0xf5800B1a93C4b0A87a60E9751d1309Ce93CC0D3A");
+  // const txObj = await peeranhaUser.grantRole(PROTOCOL_ADMIN_ROLE, "0x31339c62c0a44b875297945edb93d88092b5fa91")
+  // const txObj = await peeranhaUser.grantRole(DISPATCHER_ROLE, "0xdf5C1E9B4a97C83b72E3d34f254729c39a206F6E")
+  // const txObj = await peeranhaUser.grantRole(BOT_ROLE, "0xdf5C1E9B4a97C83b72E3d34f254729c39a206F6E")
+  // const txObj = await peeranhaUser.grantRole(PROTOCOL_ADMIN_ROLE, "0x570895fd1f7d529606e495885f6eaf1924baa08e");
   // const txObj = await peeranhaUser.followCommunity(signers[0].address, 1);
   // const txObj = await peeranhaUser.unfollowCommunity(signers[0].address, 1);
-  // const txObj = await peeranhaUser.isProtocolAdmin("0x570895fd1f7d529606e495885f6eaf1924baa08e");
+  // const txObj = await peeranhaUser.banCommunityUser(signers[0].address, "0xed5aec8204ac145bb388fdf7a36dc518fab9e0f3", 2);
+  // const txObj = await peeranhaUser.unBanCommunityUser(signers[0].address, "0xed5aec8204ac145bb388fdf7a36dc518fab9e0f3", 2);
+  // const txObj = await peeranhaUser.isBanedUser(signers[0].address, 2);
+  // const txObj = await peeranhaUser.isProtocolAdmin("0x31339c62c0a44b875297945edb93d88092b5fa91");
   // const txObj = await peeranhaUser.getUserRatingCollection("0x9fBE2C1d7B0Ebeddb2faEF30Be00Ed838f19E499", 2);
   // const txObj = await peeranhaUser.getUsersCount();
+  // const txObj = await peeranhaUser.getUsersCount();
+  // const txObj = await peeranhaUser.getUserByAddress("0xd06b205d2826b481e64492f85694d07e57a17b19");
 
   console.log(`Contract: PeeranhaUser - ${USER_ADDRESS}`)
   console.log(`Submitted transaction - ${JSON.stringify(txObj)}`);
-  console.log(`Waiting for transaction confirmation`);
+  console.log(`Waiting for transaction confirmation`); 
   await txObj.wait();
   console.log('Transaction confirmed');
 }
@@ -159,6 +186,7 @@ async function communityFunctions() {
   const signers = await ethers.getSigners();
   const txObj = await peeranhaCommunity.createCommunity(signers[0].address, await getBytes32FromData(testCommunity), await getTags(5));
   // const txObj = await peeranhaCommunity.updateCommunity(signers[0].address, 3, await getBytes32FromData(testCommunity));
+  // const txObj = await peeranhaCommunity.getCommunity(2);
 
   console.log(`Submitted transaction - ${JSON.stringify(txObj)}`);
   console.log(`Waiting for transaction confirmation`);
