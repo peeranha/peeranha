61b0044b1f89438c8fa0b45062d611e6b90b8eb5
Freitag-fri <sanya007ua@gmail.com>
2024-10-05 03:05:00 +0300

    upgrade user/post lib

--- scripts/upgrade-user.js
+++ scripts/upgrade-user.js
@@ -1,6 +1,6 @@
 const { ethers, upgrades } = require("hardhat");
 const { USER_ADDRESS } = require('../env.json');
-const { getChainName, verifyContract } = require('./common-action');
+// const { getChainName, verifyContract } = require('./common-action');
 
 async function main() {
   console.log(`Peeranha user address: ${USER_ADDRESS}`)
@@ -19,16 +19,16 @@ async function main() {
   const peeranhaUser = await upgrades.upgradeProxy(USER_ADDRESS, PeeranhaUser, {unsafeAllowLinkedLibraries: true, timeout: 0});
   console.log("Peeranha User upgraded at address:", peeranhaUser.address);
   
-  const userLibProxyAddress = await userLib.resolvedAddress;
-  const userProxyAddress = `0x${peeranhaUser.deployTransaction.data.slice(98)}`;
-  const chainId = JSON.stringify(PeeranhaUser.signer.provider._network.chainId);
+  // const userLibProxyAddress = await userLib.resolvedAddress;
+  // const userProxyAddress = `0x${peeranhaUser.deployTransaction.data.slice(98)}`;
+  // const chainId = JSON.stringify(PeeranhaUser.signer.provider._network.chainId);
 
-  const chainName = await getChainName(chainId);
-  console.log(`Verify User contract: ${userProxyAddress} Chain Id: ${chainId}, chain name: ${chainName}`);
-  await verifyContract(userProxyAddress, chainName);
+  // const chainName = await getChainName(chainId);
+  // console.log(`Verify User contract: ${userProxyAddress} Chain Id: ${chainId}, chain name: ${chainName}`);
+  // await verifyContract(userProxyAddress, chainName);
 
-  console.log(`Verify UserLib contract: ${userLibProxyAddress} Chain Id: ${chainId}, chain name: ${chainName}`);
-  await verifyContract(userLibProxyAddress, chainName);
+  // console.log(`Verify UserLib contract: ${userLibProxyAddress} Chain Id: ${chainId}, chain name: ${chainName}`);
+  // await verifyContract(userLibProxyAddress, chainName);
 }
 
 main()
