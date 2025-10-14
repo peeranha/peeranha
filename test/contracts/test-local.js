a24039cb96856019c9188c1e72843349969470b9
Freitag-fri <sanya007ua@gmail.com>
2025-10-14 06:43:36 +0300

    ref test local script

--- test/contracts/test-local.js
+++ test/contracts/test-local.js
@@ -10,23 +10,25 @@ const {
 } = require('./utils');
 const { parseEther }  = require("ethers/lib/utils");
 
-async function createUser(peeranhaUser, verifier, user, hash) {
-	if (!(await peeranhaUser.connect(verifier).hasRole(VERIFIER_ROLE, verifier.address))) {
-		await peeranhaUser.grantRole(VERIFIER_ROLE, verifier.address);
+	async function createUser(peeranhaUser, verifier, user, hash) {
+		if (!(await peeranhaUser.connect(verifier).hasRole(VERIFIER_ROLE, verifier.address))) {
+			await peeranhaUser.grantRole(VERIFIER_ROLE, verifier.address);
+		}
+		await peeranhaUser.connect(user).createUser(user.address, hash);
+		await peeranhaUser.connect(verifier).grantRole(VERIFIED_ROLE, user.address);
 	}
-	await peeranhaUser.connect(user).createUser(user.address, hash);
-	await peeranhaUser.connect(verifier).grantRole(VERIFIED_ROLE, user.address);
-}
-
-async function createCommunities (peeranhaCommunity, wallet, countOfCommunities, communitiesIds) {
-	const ipfsHashes = await getHashesContainer(countOfCommunities);
-	await Promise.all(await communitiesIds.map(async(id) => {
-		return await peeranhaCommunity.createCommunity(wallet, ipfsHashes[id - 1], createTags(5));
-	}));
 
 
-	expect(await peeranhaCommunity.getCommunitiesCount()).to.equal(countOfCommunities)
-	// Occasionally, a verification error occurs.
+	// in utils error "ReferenceError: expect is not defined"
+	const createCommunities = async (peeranhaCommunity, wallet, countOfCommunities, communitiesIds) => {
+		const ipfsHashes = getHashesContainer(countOfCommunities);
+		await Promise.all(communitiesIds.map(async(id) => {
+			return await peeranhaCommunity.createCommunity(wallet, ipfsHashes[id - 1], createTags(5));
+		}));
+
+		expect(await peeranhaCommunity.getCommunitiesCount()).to.equal(countOfCommunities)
+
+		// Occasionally, a verification error occurs.
         // Communities are created successfully, but during the verification step,
         // they are returned in a non-deterministic order, as is the value being checked.
         // The issue reproduces approximately 1 in 5 runs.
@@ -34,7 +36,8 @@ async function createCommunities (peeranhaCommunity, wallet, countOfCommunities,
         // await Promise.all(communitiesIds.map(async(id) => {
         //     const community = await peeranhaCommunity.getCommunity(id);
         //     return await expect(community.ipfsDoc.hash).to.equal(ipfsHashes[id - 1]);
-    // }));
+        // }));
+	}
 
 describe("Test local", function () {
 	
